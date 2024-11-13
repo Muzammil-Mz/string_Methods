0960f4f87a2806752a85e4c51d012837b62b6a23
Muzammil-Mz <Mujjuimran74@gmail.com>
2024-11-13 17:28:39 +0000

    13

--- 1.js
+++ 1.js
@@ -1,4 +1,4 @@
-//at shows string char persrtn at specific index
+//at shows string char persrtn at specific index also accepts negativr indexes where as charat doesnt accept gives output
 
 let students="Hello worold"
 console.log(students.at(3));
---
+++ 2.js
@@ -0,0 +1,4 @@
+//charat - shows character present at specific doesnt accept negative index gives empty
+
+let students="Hello world"
+console.log(students.charAt(-3));
