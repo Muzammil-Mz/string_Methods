99525ae1782854337674995e89d1f3e00eabe094
Muzammil-Mz <Mujjuimran74@gmail.com>
2024-11-13 19:23:32 +0000

    13

---
+++ 19.js
@@ -0,0 +1,12 @@
+// //search  if presenr gives index of whre its presrent  if not gives -1
+// let namee="hello im not feeling well"
+// console.log(namee.search("not"));
+
+
+
+// let namee="hello im not feeling well"
+// console.log(namee.search("0"));
+
+
+// let namee="hello im not feeling well"
+// console.log(namee.search(" ")); //5
---
+++ 20.js
@@ -0,0 +1,11 @@
+//includes
+// let student="kisore junaidali"
+// console.log(student.includes("Ali")); //false
+
+
+// let student="kisore junaidali"
+// console.log(student.includes("ali")); //true
+
+
+// let student="kisore junaidali"
+// console.log(student.includes("Ali")); false
