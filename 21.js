cc0df1518dd9badd20e7cd35d59ee9bc22e16196
Muzammil-Mz <Mujjuimran74@gmail.com>
2024-11-13 19:27:16 +0000

    13

---
+++ 21.js
@@ -0,0 +1,11 @@
+// match return nukll if not present if present reutns in the form of array
+// let sam="hey im checkcing"
+// console.log(sam.match(/chek/g));
+
+
+// let sam="hey im checkcing"
+// console.log(sam.match(/check/g)); 
+
+
+// let sam="hey im checkcing"
+// console.log(sam.match(/Check/i)); //check for insensitive
---
+++ 22.js
@@ -0,0 +1,8 @@
+//split -- splits string in to array
+
+// let studemt="ali ali"
+// console.log(studemt.split());
+
+let hey="helllo ooooooooooooooooooooooooooo"
+
+console.log(hey.split());
