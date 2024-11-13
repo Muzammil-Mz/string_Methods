70579cb1d8c8b8dc18cd16b6c029e20b5b26d1b4
Muzammil-Mz <Mujjuimran74@gmail.com>
2024-11-13 19:19:41 +0000

    13

---
+++ 17.js
@@ -0,0 +1,14 @@
+//repale - replaces specific elemtn from the string 
+
+// let month="January"
+// console.log(month.replace("January","february"))
+
+
+// let month="January"
+// console.log(month.replace("Jan","feb")) //ans is febuary
+
+
+let month="January"
+console.log(month.replace("feb")) //January
+
+
---
+++ 18.js
@@ -0,0 +1,11 @@
+// ..index of gives index number of specified value here gave i 
+// let indexx="indeeee"
+// console.log(indexx.indexOf("i"));
+
+
+// let indexx="indeeee"
+// console.log(indexx.indexOf("i"));
+
+// let indexx="indeeee"
+// console.log(indexx.indexOf("i"));
+
